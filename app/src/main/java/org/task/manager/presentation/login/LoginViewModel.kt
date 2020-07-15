0bbd9f970e109bcadc848c8cd7933f822c06fbdd
Steampunk1543 <dasies11@gmail.com>
2020-07-15 23:57:50 +0200

    Add result to manage errors and update live data object

--- app/src/main/java/org/task/manager/presentation/login/LoginViewModel.kt
+++ app/src/main/java/org/task/manager/presentation/login/LoginViewModel.kt
@@ -4,42 +4,48 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import org.task.manager.data.network.model.AuthenticationResult
 import org.task.manager.data.network.model.request.LoginRequest
+import org.task.manager.data.network.model.response.AuthenticationState
+import org.task.manager.domain.Result
 import org.task.manager.domain.interactor.LoginUseCase
 
 class LoginViewModel(private val loginUseCase: LoginUseCase) : ViewModel() {
 
-    enum class AuthenticationState {
-        AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
-    }
-
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
 
-    val authenticationState = MutableLiveData<AuthenticationState>()
+    val authenticationResult = MutableLiveData<AuthenticationResult>()
     var username: String
 
     init {
         // In this example, the user is always unauthenticated when MainActivity is launched
-        authenticationState.value = AuthenticationState.UNAUTHENTICATED
+        authenticationResult.value?.state = AuthenticationState.UNAUTHENTICATED
         username = ""
     }
 
     fun refuseAuthentication() {
-        authenticationState.value = AuthenticationState.UNAUTHENTICATED
+        authenticationResult.value?.state = AuthenticationState.UNAUTHENTICATED
     }
 
     fun authenticate(username: String, password: String, isRemember: Boolean) {
+
         val loginRequest = LoginRequest(username, password, isRemember)
         coroutineScope.launch {
-          val result = loginUseCase.execute(loginRequest)
-            if (result) {
-//                this.username = username
-                authenticationState.postValue(AuthenticationState.AUTHENTICATED)
-            } else {
-                authenticationState.postValue(AuthenticationState.INVALID_AUTHENTICATION)
+            when (val result = loginUseCase(loginRequest)) {
+                is Result.Success -> authenticationResult.postValue(AuthenticationResult(AuthenticationState.AUTHENTICATED, "Ok"))
+                is Result.Error ->
+                    result.throwable.message?.let {
+                        authenticationResult.postValue(AuthenticationResult(AuthenticationState.INVALID_AUTHENTICATION, it))
+                    }
             }
         }
     }
+
+    public override fun onCleared() {
+        coroutineScope.cancel()
+    }
+
 
 }
