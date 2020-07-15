aaec4c30bbf12dde8e1f9d641081e9d3d4bd733b
Steampunk1543 <dasies11@gmail.com>
2020-07-15 23:56:29 +0200

    Update login flow elements

--- app/src/main/java/org/task/manager/domain/interactor/LoginUseCase.kt
+++ app/src/main/java/org/task/manager/domain/interactor/LoginUseCase.kt
@@ -1,6 +1,8 @@
 package org.task.manager.domain.interactor
 
 import org.task.manager.data.network.model.request.LoginRequest
+import org.task.manager.data.network.model.response.LoginResponse
+import org.task.manager.domain.Result
 import org.task.manager.domain.repository.LoginRepository
 import org.task.manager.shared.SessionManager
 
@@ -8,10 +10,9 @@ class LoginUseCase(private val loginRepository: LoginRepository,
                    private val sessionManager: SessionManager) {
 
 
-    suspend fun execute(request: LoginRequest): Boolean {
-        val response = loginRepository.login(request)
-        sessionManager.saveAuthToken(response.authToken)
-        return true
+    suspend operator fun invoke(request: LoginRequest): Result<LoginResponse> {
+//        sessionManager.saveAuthToken("")
+        return loginRepository.login(request)
     }
 
 }
--- app/src/main/java/org/task/manager/domain/repository/LoginRepository.kt
+++ app/src/main/java/org/task/manager/domain/repository/LoginRepository.kt
@@ -2,7 +2,8 @@ package org.task.manager.domain.repository
 
 import org.task.manager.data.network.model.request.LoginRequest
 import org.task.manager.data.network.model.response.LoginResponse
+import org.task.manager.domain.Result
 
 interface LoginRepository {
-    suspend fun login(request: LoginRequest): LoginResponse
+    suspend fun login(request: LoginRequest): Result<LoginResponse>
 }
