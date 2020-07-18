03e1f2141e1037047d0aa6a93d3a82629d08615c
Steampunk1543 <dasies11@gmail.com>
2020-07-18 12:17:54 +0200

    Change find view by id to synthetic

--- app/src/main/java/org/task/manager/presentation/login/LoginFragment.kt
+++ app/src/main/java/org/task/manager/presentation/login/LoginFragment.kt
@@ -4,52 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_login.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.task.manager.R
-import org.task.manager.data.network.model.response.AuthenticationState
+import org.task.manager.domain.model.AuthenticationState
 import org.task.manager.databinding.FragmentLoginBinding
 import org.task.manager.hide
 import org.task.manager.presentation.view.ViewElements
 import org.task.manager.show
 
 class LoginFragment : Fragment(), ViewElements {
-    // Get a reference to the ViewModel scoped to this Fragment
     private val viewModel: LoginViewModel by viewModel()
     private lateinit var binding: FragmentLoginBinding
-
-    private lateinit var usernameEditText: EditText
-    private lateinit var passwordEditText: EditText
-    private lateinit var rememberMeCheckBox: CheckBox
+    private lateinit var navController: NavController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        navController = findNavController()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val navController = findNavController()
-
-        usernameEditText = view.findViewById(R.id.username)
-        passwordEditText = view.findViewById(R.id.new_password)
-        rememberMeCheckBox = view.findViewById(R.id.rememberMe)
-
         binding.loginButton.setOnClickListener {
             showProgress()
-            viewModel.authenticate(usernameEditText.text.toString(),
-                passwordEditText.text.toString(), rememberMeCheckBox.isActivated)
+            viewModel.authenticate(username.text.toString(), password.text.toString(), rememberMe.isActivated)
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
@@ -59,9 +47,9 @@ class LoginFragment : Fragment(), ViewElements {
 
         viewModel.authenticationResult.observe(viewLifecycleOwner, Observer { authenticationResult ->
             when (authenticationResult.state) {
-                AuthenticationState.AUTHENTICATED -> navController.popBackStack()
-                AuthenticationState.UNAUTHENTICATED -> navController.popBackStack()
-                AuthenticationState.INVALID_AUTHENTICATION -> navController.popBackStack()
+                AuthenticationState.AUTHENTICATED -> authenticatedUser(authenticationResult.message)
+                AuthenticationState.INVALID_AUTHENTICATION -> showMessage(authenticationResult.message)
+                AuthenticationState.UNAUTHENTICATED ->  navController.popBackStack()
             }
             hideProgress()
         })
@@ -77,6 +65,11 @@ class LoginFragment : Fragment(), ViewElements {
 
     override fun hideProgress() {
         progressBar.hide()
+    }
+
+    private fun authenticatedUser(message: String) {
+        showMessage(message)
+        navController.popBackStack()
     }
 
 }
