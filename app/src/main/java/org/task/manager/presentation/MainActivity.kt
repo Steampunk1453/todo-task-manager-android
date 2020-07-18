7be65836f3e5127eb35eed59528a362fc649a625
Steampunk1543 <dasies11@gmail.com>
2020-07-18 12:12:33 +0200

    Remove and change to use timber

--- app/src/main/java/org/task/manager/presentation/MainActivity.kt
+++ app/src/main/java/org/task/manager/presentation/MainActivity.kt
@@ -1,12 +1,19 @@
 package org.task.manager.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.task.manager.BuildConfig
 import org.task.manager.R
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
         setContentView(R.layout.activity_main)
     }
 }
