bb4667e8be25b9dbcd6ace8feab310f23b229aea
Steampunk1543 <dasies11@gmail.com>
2020-08-01 18:20:06 +0200

    Add new di for shared data between views

--- app/src/main/java/org/task/manager/presentation/di/PresentationModule.kt
+++ app/src/main/java/org/task/manager/presentation/di/PresentationModule.kt
@@ -1,15 +1,17 @@
 package org.task.manager.presentation.di
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
-import org.task.manager.presentation.login.LoginViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.task.manager.presentation.registration.RegistrationViewModel
+import org.task.manager.presentation.auth.login.LoginViewModel
+import org.task.manager.presentation.auth.registration.RegistrationViewModel
+import org.task.manager.presentation.shared.SharedViewModel
 
 val module: Module = module {
 
     viewModel { LoginViewModel(get(), get()) }
     viewModel { RegistrationViewModel(get()) }
+    viewModel { SharedViewModel() }
 
 }
 
