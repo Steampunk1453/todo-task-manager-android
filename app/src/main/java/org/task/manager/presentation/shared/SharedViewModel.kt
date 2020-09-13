4c1bad942cf3db1b39f1024fedd9ddf99c9bed3a
Steampunk <dasies11@gmail.com>
2020-09-14 00:03:01 +0200

    Share data between fragments without call to server

--- app/src/main/java/org/task/manager/presentation/shared/SharedViewModel.kt
+++ app/src/main/java/org/task/manager/presentation/shared/SharedViewModel.kt
@@ -2,13 +2,18 @@ package org.task.manager.presentation.shared
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.task.manager.domain.model.Audiovisual
 
 class SharedViewModel : ViewModel() {
     val userData = MutableLiveData<String>()
+    val audiovisual = MutableLiveData<Audiovisual>()
 
     fun setUserName(username: String) {
         userData.value = username
     }
 
+    fun sendAudiovisual(obj: Audiovisual) {
+        audiovisual.value = obj
+    }
 
 }
