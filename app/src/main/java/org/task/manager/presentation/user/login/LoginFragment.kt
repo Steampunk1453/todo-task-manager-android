3a6d35066930ea40cd1eb4e880f0135fd878c800
Steampunk <dasies11@gmail.com>
2020-10-24 11:25:42 +0200

    Polish code

--- app/src/main/java/org/task/manager/presentation/user/login/LoginFragment.kt
+++ app/src/main/java/org/task/manager/presentation/user/login/LoginFragment.kt
@@ -25,7 +25,7 @@ import org.task.manager.presentation.view.ViewElements
 import org.task.manager.show
 
 class LoginFragment : Fragment(), ViewElements {
-    private val viewModel: LoginViewModel by viewModel()
+    private val loginViewModel: LoginViewModel by viewModel()
     private lateinit var sharedViewModel: SharedViewModel
     private lateinit var binding: FragmentLoginBinding
     private lateinit var  navController: NavController
@@ -44,15 +44,15 @@ class LoginFragment : Fragment(), ViewElements {
 
         binding.login.setOnClickListener {
             showProgress()
-            viewModel.authenticate(username.text.toString(), password.text.toString(), rememberMe.isActivated)
+            loginViewModel.authenticate(username.text.toString(), password.text.toString(), rememberMe.isActivated)
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
-            viewModel.refuseAuthentication()
+            loginViewModel.refuseAuthentication()
             navController.navigate(R.id.fragment_main)
         }
 
-        viewModel.authenticationResult.observe(viewLifecycleOwner, { authenticationResult ->
+        loginViewModel.authenticationResult.observe(viewLifecycleOwner, { authenticationResult ->
             when (authenticationResult.state) {
                 AuthenticationState.AUTHENTICATED -> authenticatedUser(authenticationResult.message)
                 AuthenticationState.INVALID_AUTHENTICATION -> showMessage(authenticationResult.message)
