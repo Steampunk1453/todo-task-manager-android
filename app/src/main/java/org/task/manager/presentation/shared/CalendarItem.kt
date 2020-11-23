bba09626e1fff653b194851e897496da920bfa33
Steampunk <dasies11@gmail.com>
2020-11-24 00:02:26 +0100

    Change types to adapt to view fields

--- app/src/main/java/org/task/manager/presentation/shared/CalendarItem.kt
+++ app/src/main/java/org/task/manager/presentation/shared/CalendarItem.kt
@@ -1,15 +1,15 @@
 package org.task.manager.presentation.shared
 
 import androidx.annotation.ColorRes
-import java.time.LocalDateTime
+import androidx.annotation.DrawableRes
 
 data class CalendarItem(
     val id: Long,
     val title: String,
-    val startDate: LocalDateTime,
-    val deadline: LocalDateTime,
-    val check: Int,
+    val startDate: String,
+    val deadline: String,
     val itemType: ItemType,
-    @ColorRes val color: Int
+    @ColorRes val color: Int,
+    @DrawableRes val icon: Int,
 )
 
