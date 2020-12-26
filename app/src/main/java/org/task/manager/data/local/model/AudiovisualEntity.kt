ccc56b255c1652a436971f860a9bb78e53bfdf1d
Steampunk <dasies11@gmail.com>
2020-12-26 19:34:40 +0100

    Formatted code

--- app/src/main/java/org/task/manager/data/local/model/AudiovisualEntity.kt
+++ app/src/main/java/org/task/manager/data/local/model/AudiovisualEntity.kt
@@ -7,27 +7,26 @@ import org.task.manager.domain.model.User
 
 @Entity(tableName = "Audiovisual")
 data class AudiovisualEntity(
-    @PrimaryKey
-    val id: Long?,
-    val title: String,
-    val genre: String,
-    val platform: String,
-    val platformUrl: String,
-    val startDate: String,
-    val deadline: String,
-    val check: Int,
-    val userId: Long?
+        @PrimaryKey
+        val id: Long?,
+        val title: String,
+        val genre: String,
+        val platform: String,
+        val platformUrl: String,
+        val startDate: String,
+        val deadline: String,
+        val check: Int,
+        val userId: Long?
 )
 
 fun AudiovisualEntity.toDomain(): Audiovisual = Audiovisual(
-    id, title, genre, platform, platformUrl,
-    startDate, deadline, check, toUserDomain()
+        id, title, genre, platform, platformUrl,
+        startDate, deadline, check, toUserDomain()
 )
 
 fun AudiovisualEntity.toUserDomain(): User = User(userId)
 
-
 fun Audiovisual.toEntity(): AudiovisualEntity = AudiovisualEntity(
-    id, title, genre, platform, platformUrl,
-    startDate, deadline, check, user?.id
+        id, title, genre, platform, platformUrl,
+        startDate, deadline, check, user?.id
 )
