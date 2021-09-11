0cc2509a2d83b69669242d965a9fd319e0642c33
Steampunk <dasies11@gmail.com>
2021-09-11 14:31:40 +0200

    Update integration tests with new title changes

--- app/src/androidTest/java/org/task/manager/instrumented/integration/stub/TitleStubIT.kt
+++ app/src/androidTest/java/org/task/manager/instrumented/integration/stub/TitleStubIT.kt
@@ -6,21 +6,42 @@ import kotlin.random.Random
 open class TitleStubIT {
 
     companion object {
-            fun getTitle(
-                    id: Long = Random.nextLong(1, 500),
-                    name: String = "name"
-            ) = Title(
-                    id,
-                    name,
-            )
 
-            fun getTitle1(
-                    id: Long = Random.nextLong(501, 1000),
-                    name: String = "name1",
-            ) = Title(
-                    id,
-                    name,
-            )
+        fun getTitle(
+            id: String = "id",
+            title: String = "title",
+            rank: Int = Random.nextInt(1, 200),
+            type: String = "type",
+            genres: String = "genres",
+            platform: String = "platform",
+            website: String = "website",
+        ) = Title(
+            id,
+            title,
+            rank,
+            type,
+            genres,
+            platform,
+            website
+        )
+
+        fun getTitle1(
+            id: String = "id1",
+            title: String = "title1",
+            rank: Int = Random.nextInt(1, 200),
+            type: String = "type1",
+            genres: String = "genres1",
+            platform: String = "platform1",
+            website: String = "website1",
+        ) = Title(
+            id,
+            title,
+            rank,
+            type,
+            genres,
+            platform,
+            website
+        )
     }
 
 }
